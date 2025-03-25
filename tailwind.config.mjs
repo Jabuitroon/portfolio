2efc69a74848282e8c8ce0948217b60a782e4592
Jabuitroon <Jandresbuiitron@gmail.com>
2025-03-24 20:44:57 -0500

    Optimización de imágenes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import formsPlugin from 'tailwindcss-animated'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [require('tailwindcss-animated')],
+	plugins: [
+		formsPlugin
+	]
 }
