3f781edc603e2f04d36c3bda554d28e3d3ffa742
Jabuitroon <Jandresbuiitron@gmail.com>
2026-01-20 20:48:39 -0500

    Config de paleta de colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 import formsPlugin from 'tailwindcss-animated'
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		formsPlugin
-	]
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        textcolor: '#1282A2',
+        primary: '#034078',
+        secundary: '#001F54',
+        dark: '#0A1128',
+      },
+    },
+  },
+  plugins: [formsPlugin],
 }
